a2aa68528739e4dbfdb81e9e4c0e7dac16e3e6a5
helloAsAlways <82492567+helloAsAlways@users.noreply.github.com>
2026-06-16 00:38:04 +0900

    Create Class.cs

---
+++ Homework4/Class.cs
@@ -0,0 +1,15 @@
+namespace my_project
+{
+    class Class
+    {
+        private int area;
+        private int capacity;
+
+        public Class(int area, int capacity)
+        {
+            this.area = area;
+            this.capacity = capacity;
+        }
+        
+    }
+}
